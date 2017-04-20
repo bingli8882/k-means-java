540900f0e4abaa3f20e733d0a1264cb81a73448c
Bing Li <bingli8882@gmail.com>
2017-04-20 15:05:32 -0400

    Create Euclidean distance function

---
+++ src/main/java/Euclidean.java
@@ -0,0 +1,14 @@
+/**
+ * Euclidean distance function
+ */
+public class Euclidean implements DistanceFunction {
+    public double compute(double[] a, double[] b) throws LengthUnmatchedException {
+        if (a.length != b.length) throw new LengthUnmatchedException("A:" + a.length + " B:" + b.length);
+        double t, s = 0;
+        for (int i = 0; i < a.length; i++) {
+            t = a[i] - b[i];
+            s += t * t;
+        }
+        return Math.sqrt(s);
+    }
+}
