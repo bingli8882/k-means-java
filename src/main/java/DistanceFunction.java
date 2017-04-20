b84be7b40281f7daa9ba4c666ec8aec935c93189
Bing Li <bingli8882@gmail.com>
2017-04-20 14:46:19 -0400

    Create DistanceFunction interface.

---
+++ src/main/java/DistanceFunction.java
@@ -0,0 +1,12 @@
+/**
+ * The interface of all distance function
+ */
+public interface DistanceFunction {
+    /**
+     * Compute the distance between two given points.
+     * @param a A double array representing one point's coordinate.
+     * @param b A double array representing the other point's coordinate.
+     * @return A double value representing the distance between two given points.
+     */
+    double compute(double[] a, double[] b);
+}
