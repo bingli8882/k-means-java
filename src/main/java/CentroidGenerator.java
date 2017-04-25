19f0ff6c3e19526fff02e648b3e8d4827f933793
Bing Li <bingli8882@gmail.com>
2017-04-25 18:55:24 -0400

    add EmptyDataSetException in CentroidGenerator interface

--- src/main/java/CentroidGenerator.java
+++ src/main/java/CentroidGenerator.java
@@ -8,5 +8,5 @@ public interface CentroidGenerator {
      * @param data The data set containing all data points.
      * @return A list of all generated centroids.
      */
-    double[][] generate(int k, double[][] data);
+    double[][] generate(int k, double[][] data) throws EmptyDataSetException;
 }
