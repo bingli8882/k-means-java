a6bc018cf422360c239e956c4dd5d8a6d0b31e1a
Bing Li <bingli8882@gmail.com>
2017-04-25 18:54:00 -0400

    Create EmptyDataSetException class

---
+++ src/main/java/EmptyDataSetException.java
@@ -0,0 +1,8 @@
+/**
+ * This exception represents the given data set is empty.
+ */
+public class EmptyDataSetException extends Exception {
+    public EmptyDataSetException(String s) {
+        super(s);
+    }
+}
