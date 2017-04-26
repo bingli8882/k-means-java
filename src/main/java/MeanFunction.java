495225e55723018f3a3d9cdbe8acc98b1c86a87d
Bing Li <bingli8882@gmail.com>
2017-04-25 22:59:20 -0400

    create MeanFunction interface

---
+++ src/main/java/MeanFunction.java
@@ -0,0 +1,11 @@
+/**
+ * This is the interface of all Mean calculation functions.
+ */
+public interface MeanFunction {
+    /**
+     * Calculate the mean of the given observations set
+     * @param data the data set
+     * @return a double array representing the mean of the given data set.
+     */
+    double[] compute(final double[][] data) throws EmptyDataSetException;
+}
