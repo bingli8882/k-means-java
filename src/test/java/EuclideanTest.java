c59825d2eb7d7d6a04ecc74d875fbeb907263837
Bing Li <bingli8882@gmail.com>
2017-04-25 23:04:08 -0400

    Add the exception test in EuclideanTest

--- src/test/java/EuclideanTest.java
+++ src/test/java/EuclideanTest.java
@@ -12,4 +12,10 @@ public class EuclideanTest {
         double[] b = {4.0,0};
         assertEquals(5.0,d.compute(a,b),0.0001);
     }
+    @Test(expected = LengthUnmatchedException.class)
+    public void testUnmatchedInputs()throws LengthUnmatchedException{
+        double[] a = {1,2,3,4,5};
+        double[] b = {1,2,3};
+        new Euclidean().compute(a,b);
+    }
 }
