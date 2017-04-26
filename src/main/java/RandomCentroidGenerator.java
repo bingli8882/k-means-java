d90a14c8ed33a9dc08a76bbbaeb57dab91ec0f7d
Bing Li <bingli8882@gmail.com>
2017-04-25 22:43:58 -0400

    create RandomCentroidGenerator and RandomCentroidGeneratorTest

---
+++ src/main/java/RandomCentroidGenerator.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Random generate centroids
+ */
+public class RandomCentroidGenerator implements CentroidGenerator {
+    public double[][] generate(int k, double[][] data) throws EmptyDataSetException {
+        if (data.length == 0 || data[0].length == 0) throw new EmptyDataSetException("The input data set is empty.");
+        if (data.length < k) throw new EmptyDataSetException("The input data set size is less than K.");
+
+        double[][] result = new double[k][data[0].length];
+
+        List<Integer> list = new ArrayList<Integer>();
+        int index = 0, t;
+
+        while (index < k) {
+            t = (int)Math.floor(Math.random()*data.length);
+            if(!list.contains(t)){
+                list.add(t);
+                for (int i = 0; i < data[t].length; i++) {
+                    result[index][i] = data[t][i];
+                }
+                index++;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/RandomCentroidGeneratorTest.java
@@ -0,0 +1,46 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by bing on 4/25/17.
+ */
+public class RandomCentroidGeneratorTest {
+
+    double[][] a = {{0,1,2},{1,2,3},{2,3,4},{3,4,5},{4,5,6}};
+    CentroidGenerator cg = new RandomCentroidGenerator();
+
+    @Test(expected = EmptyDataSetException.class)
+    public void testEmptyInput() throws EmptyDataSetException{
+        cg.generate(10,new double[3][4]);
+    }
+    @Test(expected = EmptyDataSetException.class)
+    public void smallSizeData() throws EmptyDataSetException{
+        cg.generate(10,a);
+    }
+    @Test
+    public void test() throws EmptyDataSetException{
+        double[][] r = cg.generate(2,a);
+        assertEquals(2,r.length);
+        assertEquals(3,r[0].length);
+
+        List<Integer> list = new ArrayList<Integer>();
+
+        for (double[] e:r) {
+            assertEquals(false,list.contains((int)e[0]));
+            list.add((int)e[0]);
+            assertEquals(true,contains(a,e));
+        }
+
+    }
+    private boolean contains(double[][] a,double[] b){
+        for (int i = 0; i < a[(int)b[0]].length; i++) {
+            if(a[(int)b[0]][i]!=b[i]) return false;
+        }
+        return true;
+    }
+
+}
